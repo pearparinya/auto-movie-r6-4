99273b4ae55ba4c36de1d6bb3b792e96dd108709
pearparinya <pearparinya@gmail.com>
2026-09-24 22:49:39 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,15 @@ android {
         versionCode = 64
         versionName = "6.4"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {

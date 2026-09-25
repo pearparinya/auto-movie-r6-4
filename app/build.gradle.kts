ef58ad972c46fc7c40be7b48ce18ead347823629
pearparinya <pearparinya@gmail.com>
2026-09-25 20:59:21 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         minSdk = 26
         targetSdk = 35
 
-        versionCode = 65
-        versionName = "6.5"
+        versionCode = 66
+        versionName = "6.6"
     }
 
     compileOptions {

73d328bd061002a09dfac4484c64fbc8a5a78e1e
pearparinya <pearparinya@gmail.com>
2026-09-26 19:26:20 +0700

    Release R6.14 consolidated workflow fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pearparinya.automovie"
         minSdk = 26
         targetSdk = 35
-        versionCode = 613
-        versionName = "6.13"
+        versionCode = 614
+        versionName = "6.14"
     }
 
     sourceSets {

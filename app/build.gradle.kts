cdf21b8a3b6a53383571a5c81f84e35bceab36ad
pearparinya <pearparinya@gmail.com>
2026-09-26 22:17:56 +0700

    Release R6.20 with live seconds clock

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pearparinya.automovie"
         minSdk = 26
         targetSdk = 35
-        versionCode = 619
-        versionName = "6.19"
+        versionCode = 620
+        versionName = "6.20"
     }
 
     sourceSets {

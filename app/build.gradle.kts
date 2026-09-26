a9f8503b796cb7080f30871637edb7cc07cfea94
pearparinya <pearparinya@gmail.com>
2026-09-26 15:20:22 +0700

    Release R6.9 with functional in-app updater

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pearparinya.automovie"
         minSdk = 26
         targetSdk = 35
-        versionCode = 68
-        versionName = "6.8"
+        versionCode = 69
+        versionName = "6.9"
     }
 
     sourceSets {

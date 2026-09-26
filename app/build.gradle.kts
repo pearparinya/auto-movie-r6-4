251f75af2b4c3dd8bf1ebfbb79009bf48a7d6951
pearparinya <pearparinya@gmail.com>
2026-09-26 21:17:39 +0700

    Release R6.18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pearparinya.automovie"
         minSdk = 26
         targetSdk = 35
-        versionCode = 617
-        versionName = "6.17"
+        versionCode = 618
+        versionName = "6.18"
     }
 
     sourceSets {
